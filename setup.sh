1de2d921bed1f5694cddcf99b645d4b31f47b8fc
Rahul Parundekar <rparundekar@gmail.com>
2024-02-19 21:13:51 -0800

    Pin llama-index

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python -m venv venv
 source venv/bin/activate
+alias python=python3
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m ipykernel install --user --name=venv
